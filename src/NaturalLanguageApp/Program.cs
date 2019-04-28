2b21105090f257790aeb55c4199181163f4e02f3
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2019-04-28 10:08:11 +0100

    add NaturalLanguageApp project files

---
+++ src/NaturalLanguageApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NaturalLanguageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi from nlAPP");
+        }
+    }
+}
