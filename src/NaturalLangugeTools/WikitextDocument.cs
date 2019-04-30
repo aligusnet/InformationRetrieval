a095bfab18586ec93e8da5907f694d3972ce3144
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2019-04-30 23:55:47 +0100

    add initial implementation of WikitextParser

---
+++ src/NaturalLangugeTools/WikitextDocument.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace NaturalLangugeTools
+{
+    /// <summary>
+    /// Represent Wikitext Document
+    /// </summary>
+    public class WikitextDocument
+    {
+        public IEnumerable<IWikitextElement> Elements { get; set; }
+    }
+
+    /// <summary>
+    /// Base interface to represent a single element of Wikitext
+    /// </summary>
+    public interface IWikitextElement
+    {
+    }
+
+    /// <summary>
+    /// WikitextElement that contains string value
+    /// </summary>
+    public interface IWikitextValue : IWikitextElement
+    {
+        string Value { get; }
+    }
+
+    public class WikitextHeader1 : IWikitextValue
+    {
+        public string Value { get; set; }
+    }
+
+    public class WikitextPlainText : IWikitextValue
+    {
+        public string Value { get; set; }
+    }
+}
---
+++ src/NaturalLangugeTools/WikitextParser.cs
@@ -0,0 +1,50 @@
+﻿using Sprache;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NaturalLangugeTools
+{
+    public static class WikitextParser
+    {
+        public static WikitextDocument Parse(string wikitext)
+        {
+            return new WikitextDocument() { Elements = TermList.Parse(wikitext) };
+        }
+
+        static readonly Parser<IEnumerable<IWikitextElement>> TermList =
+            from first in Sprache.Parse.WhiteSpace.Many().Optional()
+            from terms in FormattedText.DelimitedBy(Sprache.Parse.LineEnd)
+            from rest in Sprache.Parse.WhiteSpace.Many().Optional().End()
+            select terms;
+
+        private static readonly Parser<IWikitextElement> Header1 =
+            from ot in Sprache.Parse.Char('=').Once()
+            from text in ParsePlainText(except: WikiMarkup.Or(Sprache.Parse.Char('=')))
+            from ct in Sprache.Parse.Char('=').Once()
+            select new WikitextHeader1 { Value = text };
+
+        private static readonly Parser<char> WikiMarkup =
+            Sprache.Parse.Char('{')
+            .Or(Sprache.Parse.Char('}'))
+            .Or(Sprache.Parse.Char('['))
+            .Or(Sprache.Parse.Char('['))
+            .Or(Sprache.Parse.Char('&'));
+
+        private static Parser<IWikitextElement> PlainText =
+            from text in ParsePlainText(except: WikiMarkup)
+                select new WikitextPlainText { Value = text };
+
+        private static readonly Parser<IWikitextElement> FormattedText =
+            Header1
+            .Or(PlainText);
+
+        private static Parser<string> ParsePlainText(Parser<char> except)
+        {
+            return
+                from text in Sprache.Parse.AnyChar.Except(except).AtLeastOnce().Text()
+                select text.Trim();
+        }
+
+    }
+
+}
