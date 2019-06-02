f3fc71254310ad6fe05259ae9a86368ac260acb7
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2019-06-02 08:21:17 +0100

    implement NumberedDocumentDataSerializer

---
+++ src/DocumentStorage/NumberedDocumentDataSerializer.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace DocumentStorage
+{
+    public class NumberedDocumentDataSerializer : IDocumentDataSerializer<int[]>
+    {
+        public int[] Deserialize(Stream stream)
+        {
+            var reader = new BinaryReader(stream);
+            var size = reader.ReadInt32();
+            var data = new int[size];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = reader.ReadInt32();
+            }
+
+            return data;
+        }
+
+        public void Serialize(Stream stream, int[] data)
+        {
+            var writer = new BinaryWriter(stream);
+            writer.Write(data.Length);
+            for (int i = 0; i < data.Length; i++)
+            {
+                writer.Write(data[i]);
+            }
+
+            writer.Flush();
+        }
+    }
+}
---
+++ src/DocumentStorageUnitTests/NumberedDocumentDataSerializerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Xunit;
+
+using DocumentStorage;
+
+
+namespace DocumentStorageUnitTests
+{
+    public class NumberedDocumentDataSerializerTests
+    {
+        [Fact]
+        public void NumberedSerializeAndDeserializeTest()
+        {
+            var data = new int[] { 11, 17, 23, 8 };
+
+            var stream = new MemoryStream();
+            var serializer = new NumberedDocumentDataSerializer();
+            serializer.Serialize(stream, data);
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            var deserializedData = serializer.Deserialize(stream);
+
+            Assert.Equal(data, deserializedData);
+        }
+    }
+}
