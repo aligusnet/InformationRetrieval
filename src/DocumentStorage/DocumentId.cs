9f512c6f791b771a45291dbdf792aceb1ff07940
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2019-06-07 19:39:34 +0100

    implement DocumentId

---
+++ src/DocumentStorage/DocumentId.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DocumentStorage
+{
+    /// <summary>
+    /// DocumentId is an unique identifier of documents in a storage,
+    /// consists of 2 parts: if of the collections 
+    /// and local id which is the document's id in the collection
+    /// </summary>
+    public readonly struct DocumentId : IComparable<DocumentId>
+    {
+        private const int offset = 16;
+
+        public readonly uint Id;
+
+        public ushort CollectionId => (ushort)(Id >> offset);
+
+        public ushort LocalId => (ushort)Id;
+
+        public DocumentId(uint id) => Id = id;
+
+        public DocumentId(ushort collectionId, ushort localId)
+        {
+            Id = ((uint)collectionId << offset) + localId;
+        }
+
+        public int CompareTo(DocumentId other)
+        {
+            return Id.CompareTo(other.Id);
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{Id:X8}");
+        }
+
+        public static DocumentId FromString(string hex)
+        {
+            return new DocumentId(Convert.ToUInt32(hex, 16));
+        }
+    }
+}
---
+++ src/DocumentStorageUnitTests/DocumentIdUnitTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Xunit;
+
+using DocumentStorage;
+
+
+namespace DocumentStorageUnitTests
+{
+    public class DocumentIdUnitTests
+    {
+        [Theory]
+        [InlineData(1, 1, 65536 + 1)]
+        [InlineData(2, 11, 2*65536 + 11)]
+        [InlineData(0, 0, 0)]
+        public void DocumentIdDeconstructorTest(ushort collectionId, ushort localId, uint id)
+        {
+            var docId = new DocumentId(collectionId, localId);
+
+            Assert.Equal(id, docId.Id);
+            Assert.Equal(collectionId, docId.CollectionId);
+            Assert.Equal(localId, docId.LocalId);
+        }
+
+        [Theory]
+        [InlineData(0, 0, "00000000")]
+        [InlineData(1, 2, "00010002")]
+        [InlineData(15, 2, "000F0002")]
+        [InlineData(1458, 33124, "05B28164")]
+        public void DocumentIdFormattingTest(ushort collectionId, ushort localId, string formatted)
+        {
+            Assert.Equal(formatted, new DocumentId(collectionId, localId).ToString());
+        }
+
+        [Theory]
+        [InlineData("00000000", 0, 0)]
+        [InlineData("00010002", 1, 2)]
+        [InlineData("000F0002", 15, 2)]
+        [InlineData("05B28164", 1458, 33124)]
+        public void DocumentIdFromStringTest(string hex, ushort collectionId, ushort localId)
+        {
+            var docId = DocumentId.FromString(hex);
+            Assert.Equal(collectionId, docId.CollectionId);
+            Assert.Equal(localId, docId.LocalId);
+        }
+        
+        [Fact]
+        public void DocumentIdCanBeKeyInDictionary()
+        {
+            var docIds = new[]
+            {
+                new DocumentId(0, 0),
+                new DocumentId(11, 15),
+                new DocumentId(1000, 738),
+            };
+
+            var dic = docIds.ToDictionary(d => d, d => d.ToString());
+
+            foreach (var docId in docIds)
+            {
+                Assert.Equal(docId.ToString(), dic[docId].ToString());
+            }
+        }
+    }
+}
