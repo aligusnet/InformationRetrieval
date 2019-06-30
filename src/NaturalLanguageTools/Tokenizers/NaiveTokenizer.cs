bed60abb82edcb230baac7f6d6ae7a7734458a91
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2019-06-30 23:02:30 +0100

    implement NaiveTokenizer #13

---
+++ src/NaturalLanguageTools/Tokenizers/NaiveTokenizer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace NaturalLanguageTools.Tokenizers
+{
+    public static class NaiveTokenizer
+    {
+        private const char Space = '\u0020';
+
+        public static IEnumerable<(int From, int To)> Tokenize(IList<char> text)
+        {
+            int from = 0;
+
+            for (int i = 0; i < text.Count; ++i)
+            {
+                if (text[i] == Space)
+                {
+                    if (i - from > 0)
+                    {
+                        yield return (from, i);
+                    }
+                    
+                    from = i + 1;
+                }
+            }
+
+            if (text.Count - from > 0)
+            {
+                yield return (from, text.Count);
+            }
+        }
+    }
+}
---
+++ src/NaturalLanguageToolsUnitTests/Tokenizers/NaiveTokenizerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Xunit;
+
+using NaturalLanguageTools.Tokenizers;
+using System.Collections.Generic;
+
+namespace NaturalLanguageToolsUnitTests.Tokenizers
+{
+    public class NaiveTokenizerTests
+    {
+        [Fact]
+        public void TokenizerTests()
+        {
+            AssertEqual(new (int, int)[] { }, "");
+            AssertEqual(new[] { (0, 4) }, "word"); ;
+            AssertEqual(new[] { (0, 3), (4, 9) }, "two words");
+            AssertEqual(new[] { (0, 5), (6, 12), (15, 19) }, "extra spaces   here");
+        }
+
+        private static void AssertEqual((int From, int To)[] expected, string text)
+        {
+            Assert.Equal(expected, NaiveTokenizer.Tokenize(text.ToCharArray()).ToArray());
+        }
+    }
+}
