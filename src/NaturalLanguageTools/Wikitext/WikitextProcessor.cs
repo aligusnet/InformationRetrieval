0dca8a76bfd4420604f24e65e12de0d77aebc6b7
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2019-07-01 20:21:35 +0100

    implement wikitext transformer #13

---
+++ src/NaturalLanguageTools/Wikitext/WikitextProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using NaturalLanguageTools.Tokenizers;
+using NaturalLanguageTools.Transformers;
+using NaturalLanguageTools.Utility;
+
+namespace NaturalLanguageTools.Wikitext
+{
+    /// <summary>
+    /// Clean, tokenize, and hash wikitext documents.
+    /// </summary>
+    public class WikitextProcessor : StorageTransformer<IList<char>, IEnumerable<int>>
+    {
+        public WikitextProcessor() : base(Process)
+        {
+        }
+
+        private static IEnumerable<int> Process(IList<char> text)
+        {
+            var cleaned = WikitextCleaner.Clean(text);
+            var tokenized = StateMachineTokenizer.Tokenize(cleaned, lowerCase: true);
+            return NaiveTokenizer.Tokenize(tokenized)
+                                 .Select(ft => TextHasher.CalculateHashCode(tokenized, ft.From, ft.To));
+        }
+    }
+}
---
+++ src/NaturalLanguageToolsUnitTests/Wikitext/WikitextProcessorTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+using NaturalLanguageTools.Utility;
+using NaturalLanguageTools.Wikitext;
+using System.Collections.Generic;
+using DocumentStorage;
+
+namespace NaturalLanguageToolsUnitTests.Wikitext
+{
+    using Tokens = IList<char>;
+
+    public class WikitextProcessorTests
+    {
+        private static readonly WikitextProcessor processor = new WikitextProcessor();
+        [Fact]
+        public void ProcessorTest()
+        {
+            string text0 = @"{{template and {{subtemplate}}}}
+                            == Header ==
+                            <a href=""http://www.link.to/"">link</a>";
+            string text1 = "Some long text;!!! with  punctuations; And!!!!?";
+            var expected = new[]
+            {
+                CreateExpectedResult("header link"),
+                CreateExpectedResult("some long text with punctuations and"),
+            };
+
+            var storage = CreateStorage(text0, text1);
+
+            var processed = processor.Transform(storage).First().Documents.Select(d => d.Data).ToArray();
+
+            Assert.Equal(expected, processed);
+        }
+
+        private static IEnumerable<DocumentCollection<Tokens>> CreateStorage(params string[] texts)
+        {
+            var docs = new List<Document<Tokens>>(texts.Length);
+            for (ushort i = 0; i < texts.Length; ++i)
+            {
+                docs.Add(CreateDocument(texts[i], i));
+            };
+
+            return new List<DocumentCollection<Tokens>>
+            {
+                DocumentCollection<Tokens>.Make(0, docs),
+            };
+        }
+
+        private static Document<Tokens> CreateDocument(string text, ushort index)
+        {
+            return new Document<Tokens>(
+                    new DocumentMetadata(new DocumentId(0, index), $"Title {index}"),
+                    text.ToCharArray());
+        }
+
+        private static IEnumerable<int> CreateExpectedResult(string words)
+        {
+            return words.Split().Select(w => TextHasher.CalculateHashCode(w.AsSpan()));
+        }
+    }
+}
