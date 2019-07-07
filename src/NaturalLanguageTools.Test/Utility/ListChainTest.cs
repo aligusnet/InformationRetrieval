e7392ccac2d570e1abec06ac9efbebcda279ffc4
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2019-07-07 11:48:09 +0100

    implement ListChain #17

---
+++ src/NaturalLanguageTools.Test/Utility/ListChainTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+using Xunit;
+
+using NaturalLanguageTools.Utility;
+
+namespace NaturalLanguageTools.Test.Utility
+{
+    public class ListChainTest
+    {
+        [Fact]
+        public void ListChainCreateTest()
+        {
+            var chain = new ListChain<int>
+            {
+                new[] { 4, 5, 6, 7 },
+                new[] { 1, 2 },
+                new[] { 3 }
+            };
+
+            Assert.Equal(7, chain.Count);
+            Assert.Equal(new[] { 4, 5, 6, 7, 1, 2, 3 }, chain.ToArray());
+        }
+
+        [Fact]
+        public void ListChainSortTest()
+        {
+            var chain = new ListChain<int>
+            {
+                new[] { 4, 5, 6, 7 },
+                new[] { 1, 2 },
+                new[] { 3 }
+            };
+
+            chain.Sort((a, b) => a[0].CompareTo(b[0]));
+
+            Assert.Equal(7, chain.Count);
+            Assert.Equal(new[] { 1, 2, 3, 4, 5, 6, 7 }, chain.ToArray());
+        }
+    }
+}
---
+++ src/NaturalLanguageTools/Utility/ListChain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NaturalLanguageTools.Utility
+{
+    /// <summary>
+    /// Respresents a list of lists as one continues collection.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    public class ListChain<T> : IEnumerable<T>, IReadOnlyCollection<T>
+    {
+        private readonly List<IList<T>> chain;
+
+        public ListChain(): this(0) { }
+
+        public ListChain(int capacity)
+        {
+            chain = new List<IList<T>>(capacity);
+            Count = 0;
+        }
+
+        public void Add(IList<T> node)
+        {
+            chain.Add(node);
+            Count += node.Count;
+        }
+
+        public void Sort(Comparison<IList<T>> comparison) =>
+            chain.Sort(comparison);
+
+        public int Count { get; private set; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var node in chain)
+            {
+                foreach (var item in node)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() =>
+            GetEnumerator();
+    }
+}
