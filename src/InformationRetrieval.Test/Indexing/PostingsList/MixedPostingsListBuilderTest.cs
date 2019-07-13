eb9066417ca0b0853ce848db9c247749b49e7fc7
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2019-07-13 17:57:30 +0100

    implement MixedPostingsListBuilder

---
+++ src/InformationRetrieval.Test/Indexing/PostingsList/MixedPostingsListBuilderTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Corpus;
+using InformationRetrieval.Indexing;
+using InformationRetrieval.Indexing.PostingsList;
+
+namespace InformationRetrieval.Test.Indexing.PostingsList
+{
+    public class MixedPostingsListBuilderTest : IndexUnitTestsBase<MixedPostingsListBuilderTest.Index>
+    {
+        private const int rangeThreshold = 3;
+
+        protected override Index CreateIndex(string[][] corpus)
+        {
+            var index = new Index();
+            IndexHelper.BuildIndex(index, corpus);
+            return index;
+        }
+
+        public class Index : IBuildableIndex<string>, ISearchableIndex<string>
+        {
+            MixedPostingsListBuilder<string> builder = new MixedPostingsListBuilder<string>(rangeThreshold);
+
+            public ISearchableIndex<string> Build()
+            {
+                return this;
+            }
+
+            public IEnumerable<DocumentId> GetAll()
+            {
+                return builder.AllDocuments;
+            }
+
+            public int GetCount(string word)
+            {
+                if (builder.RangedPostingsLists.TryGetValue(word, out var blockList))
+                {
+                    return blockList.Count;
+                }
+
+                if (builder.UncompressedPostingsLists.TryGetValue(word, out var ids))
+                {
+                    return ids.Count;
+                }
+
+                return 0;
+            }
+
+            public int GetCount()
+            {
+                return builder.AllDocuments.Count;
+            }
+
+            public void IndexTerm(DocumentId id, string term, int position)
+            {
+                builder.Add(id, term);
+            }
+
+            public IEnumerable<DocumentId> Search(string word)
+            {
+                if (builder.RangedPostingsLists.TryGetValue(word, out var blockList))
+                {
+                    return blockList;
+                }
+
+                if (builder.UncompressedPostingsLists.TryGetValue(word, out var ids))
+                {
+                    return ids;
+                }
+
+                return Array.Empty<DocumentId>();
+            }
+        }
+    }
+}
---
+++ src/InformationRetrieval/Indexing/PostingsList/MixedPostingsListBuilder.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+using Corpus;
+
+namespace InformationRetrieval.Indexing.PostingsList
+{
+    /// <summary>
+    /// Builds postings lists using mixed strategy: 
+    /// ranged postings lists for frequent terms
+    /// and uncompressed postings list otherwise.
+    /// </summary>
+    /// <typeparam name="T">Term's type</typeparam>
+    public class MixedPostingsListBuilder<T>
+    {
+        private readonly int rangeThreshold;
+        public RangePostingsList AllDocuments;
+        public IDictionary<T, RangePostingsList> RangedPostingsLists;
+        public IDictionary<T, IList<DocumentId>> UncompressedPostingsLists;
+
+        public MixedPostingsListBuilder(int rangeThreshold)
+        {
+            AllDocuments = new RangePostingsList();
+            RangedPostingsLists = new Dictionary<T, RangePostingsList>();
+            UncompressedPostingsLists = new Dictionary<T, IList<DocumentId>>();
+            this.rangeThreshold = rangeThreshold;
+        }
+
+        public void Add(DocumentId id, T word)
+        {
+            if (RangedPostingsLists.TryGetValue(word, out var blockList))
+            {
+                blockList.Add(id);
+            }
+            else
+            {
+                if (!UncompressedPostingsLists.TryGetValue(word, out var ids))
+                {
+                    ids = new List<DocumentId>(1);
+                    UncompressedPostingsLists.Add(word, ids);
+                    ids.Add(id);
+                }
+                else if (ids[^1] != id)
+                {
+                    ids.Add(id);
+                }
+
+                int count = ids.Count;
+                if (count >= rangeThreshold && id.Id - ids[count - rangeThreshold].Id == (rangeThreshold - 1))
+                {
+                    MoveToIndex(word, ids);
+                }
+            }
+
+            AllDocuments.Add(id);
+        }
+
+        private void MoveToIndex(T word, IList<DocumentId> ids)
+        {
+            UncompressedPostingsLists.Remove(word);
+
+            var list = new RangePostingsList();
+            foreach (var id in ids)
+            {
+                list.Add(id);
+            }
+
+            RangedPostingsLists.Add(word, list);
+        }
+    }
+}
