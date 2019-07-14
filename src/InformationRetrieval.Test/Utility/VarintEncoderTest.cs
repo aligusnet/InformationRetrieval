15a2875e7658e4fa20a6a282282665ff6dd8494b
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2019-07-14 15:25:44 +0100

    implement VarintEncoder #19

---
+++ src/InformationRetrieval.Test/Utility/VarintEncoderTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+using Xunit;
+using InformationRetrieval.Utility;
+
+namespace InformationRetrieval.Test.Utility
+{
+    public class VarintEncoderTest
+    {
+        [Fact]
+        public void VarintEncoderSmokeTest()
+        {
+            var data = new byte[30];
+            int pos = 0;
+            pos += VarintEncoder.Encode(1023, data.AsSpan(pos));
+            pos += VarintEncoder.Encode(5, data.AsSpan(pos));
+            pos += VarintEncoder.Encode(ulong.MaxValue, data.AsSpan(pos));
+            pos += VarintEncoder.Encode(ulong.MinValue, data.AsSpan(pos));
+
+            var encoded = VarintEncoder.Decode(data, 0, pos).ToArray();
+
+            Assert.Equal(new ulong[] { 1023, 5, ulong.MaxValue, ulong.MinValue }, encoded);
+            Assert.Equal(4, VarintEncoder.GetIntegerCount(data.AsSpan()));
+        }
+    }
+}
---
+++ src/InformationRetrieval/Utility/VarintEncoder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InformationRetrieval.Utility
+{
+    public static class VarintEncoder
+    {
+        public static int Encode(ulong n, Span<byte> output)
+        {
+            const int BufferLength = 10;
+            Span<byte> buffer = stackalloc byte[BufferLength];
+            int pos = BufferLength;
+            do
+            {
+                buffer[--pos] = (byte)(n & 0x7f);
+                n >>= 7;
+            }
+            while (n != 0);
+
+            buffer[BufferLength - 1] |= 0x80;
+
+            buffer.Slice(pos, BufferLength - pos).CopyTo(output);
+
+            return BufferLength - pos;
+        }
+
+        public static IEnumerable<ulong> Decode(byte[] data, int start, int length)
+        {
+            int end = start + length;
+            ulong n = 0;
+            for (int i = start; i < end; ++i)
+            {
+                if ((data[i] & 0x80) == 0x80)
+                {
+                    n = (n << 7) + (ulong)(data[i] & 0x7f);
+                    yield return n;
+                    n = 0;
+                }
+                else
+                {
+                    n = (n << 7) + data[i];
+                }
+            }
+        }
+
+        public static int GetIntegerCount(ReadOnlySpan<byte> buffer)
+        {
+            int count = 0;
+
+            for (int i = 0; i < buffer.Length; ++i)
+            {
+                if ((buffer[i] & 0x80) == 0x80)
+                {
+                    ++count;
+                }
+            }
+
+            return count;
+        }
+    }
+}
