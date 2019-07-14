7f0702e4a93360e8b7bf04fbedec104afd18cd8f
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2019-07-14 11:17:17 +0100

    implement extandable array #19

---
+++ src/InformationRetrieval.Test/Utility/ExpandableArrayTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using InformationRetrieval.Utility;
+
+namespace InformationRetrieval.Test.Utility
+{
+    public class ExpandableArrayTest
+    {
+        [Fact]
+        public void ExpandableArraySmokeTest()
+        {
+            var array = new ExpandableArray<int> { 1, 5, 10, 11 };
+
+            Assert.Equal(4, array.Count);
+            Assert.Equal(new[] { 1, 5, 10, 11 }, array);
+
+            array[3] += 11;
+
+            Assert.Equal(22, array.Buffer[3]);
+
+            array.Add(new int[] { 2, 3, 5, 7, 11, 13, 17, 19 });
+
+            Assert.Equal(12, array.Count);
+            Assert.Equal(new[] { 1, 5, 10, 22, 2, 3, 5, 7, 11, 13, 17, 19 }, array);
+        }
+    }
+}
---
+++ src/InformationRetrieval/Utility/ExpandableArray.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace InformationRetrieval.Utility
+{
+    /// <summary>
+    /// A simple wrapper around System.Array 
+    /// intended to be almost as fast as System.Array
+    /// but expandable.
+    /// It exposes internal array unlike System.Generic.List.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements of the array.</typeparam>
+    public class ExpandableArray<T> : IReadOnlyCollection<T>
+    {
+        private T[] buffer;
+
+        public ExpandableArray() : this(0) { }
+
+        public ExpandableArray(int capacity)
+        {
+            Length = 0;
+            buffer = capacity > 0 ? new T[capacity] : Array.Empty<T>();
+        }
+
+        public int Length { get; private set; }
+
+        public T[] Buffer { get => buffer; }
+
+        public int Count => Length;
+
+        public void Add(T v)
+        {
+            if (Length >= buffer.Length)
+            {
+                Resize(Math.Max(8, buffer.Length * 2));
+            }
+
+            buffer[Length] = v;
+            ++Length;
+        }
+
+        public void Add(T[] values)
+        {
+            if (Length + values.Length > buffer.Length)
+            {
+                Resize(Math.Max(Length + values.Length, buffer.Length * 2));
+            }
+
+            Array.Copy(values, 0, buffer, Length, values.Length);
+            Length += values.Length;
+        }
+
+        public ref T this[int index] => ref buffer[index];
+
+        public void Resize(int newSize)
+        {
+            Array.Resize(ref buffer, newSize);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Length; ++i)
+            {
+                yield return buffer[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
